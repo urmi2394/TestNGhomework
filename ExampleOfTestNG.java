122921d6cc4aceeb09c79bf5923fd87ffb08c615
urmi2394 <urmi2394@gmail.com>
2022-02-25 20:48:51 +0000

    Buying Product Script

--- ExampleOfTestNG.java
+++ ExampleOfTestNG.java
@@ -1,8 +1,14 @@
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.TagName;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.internal.JsonToWebElementConverter;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
+
+import javax.swing.text.TabableView;
 
 public class ExampleOfTestNG {
     WebDriver driver;
@@ -41,7 +47,7 @@ public class ExampleOfTestNG {
     @Test(priority = 7)
     void shoppingCart() throws InterruptedException {
         driver.findElement(By.className("cart-label")).click();
-        Thread.sleep(20000);
+
     }
     @Test(priority = 8)
     void updateItem(){
@@ -56,16 +62,107 @@ public class ExampleOfTestNG {
     @Test(priority = 10)
     void TermsAndCondition() throws InterruptedException {
         driver.findElement(By.id("termsofservice")).click();
-        Thread.sleep(20000);
     }
     @Test(priority = 11)
     void checkingOut() throws InterruptedException {
         driver.findElement(By.id("checkout")).click();
-        Thread.sleep(2000);
     }
     @Test(priority = 12)
     void GuestCheckOut(){
         driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/div[1]/div[1]/div[3]/button[1]")).click();
+    }
+    @Test(priority = 14)
+    void firstName(){
+        driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/ol/li[1]/div[2]/form/div/div/div[2]/div/div/div[1]/input")).sendKeys("Urvi");
+    }
+    @Test(priority = 15)
+    void secondName(){
+        driver.findElement(By.id("BillingNewAddress_LastName")).sendKeys("Dave");
+    }
+    @Test(priority = 16)
+    void enterEmail(){
+        driver.findElement(By.id("BillingNewAddress_Email")).sendKeys("urvi0123@gmail.com");
+    }
+    @Test(priority = 17)
+    void enterCompany(){
+        driver.findElement(By.id("BillingNewAddress_Company")).sendKeys("unify");
+    }
+    @Test(priority = 18)
+    void selectCountry(){
+        WebElement country = driver.findElement(By.name("BillingNewAddress.CountryId"));
+        Select select = new Select(country);
+        select.selectByVisibleText("United Kingdom");
+    }
+    @Test(priority = 19)
+    void enterCity(){
+        driver.findElement(By.id("BillingNewAddress_City")).sendKeys("Wembley");
+    }
+    @Test(priority = 20)
+    void enterAddress(){
+        driver.findElement(By.id("BillingNewAddress_Address1")).sendKeys("6,AspenDrive");
+    }
+    @Test(priority = 21)
+    void enterPostCode(){
+        driver.findElement(By.id("BillingNewAddress_ZipPostalCode")).sendKeys("HA0 2KH");
+    }
+    @Test(priority = 22)
+    void enterMobileNumber(){
+        driver.findElement(By.id("BillingNewAddress_PhoneNumber")).sendKeys("1234567890");
+    }
+    @Test(priority = 23)
+    void clickContinue(){
+        driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/ol/li[1]/div[2]/div/button[4]")).click();
+    }
+//    @Test(priority = 24)
+//    void selectContinueShippingMethod(){
+//        driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/ol/li[3]/div[2]/form/div[2]/button")).click();
+//    }
+
+////    @Test(priority = 26)
+//    void selectCreditCard(){
+//        driver.findElement(By.id("paymentmethod_1")).click();
+//    }
+//    @Test(priority = 27)
+//    void selectContinueToPaymentMethod(){
+//        driver.findElement(By.className("button-1 payment-method-next-step-button")).click();
+//    }
+//    @Test(priority = 28)
+//    void selectCreditCardType(){
+//        WebElement card = driver.findElement(By.name("CreditCardType"));
+//        Select select2 = new Select(card);
+//        select2.selectByVisibleText("visa");
+//    }
+//    @Test(priority = 29)
+//    void cardHolderName(){
+//        driver.findElement(By.name("CardholderName")).sendKeys("Urmi Patel");
+//    }
+//    @Test(priority = 30)
+//    void cardNumber(){
+//        driver.findElement(By.id("CardNumber")).sendKeys("1234 5678 9012 3456");
+//    }
+//    @Test(priority = 31)
+//    void selectExpireMonth(){
+//        WebElement month = driver.findElement(By.id("ExpireMonth"));
+//        Select select3 = new Select(month);
+//        select3.selectByVisibleText("03");
+//    }
+//    @Test(priority = 32)
+//    void selectExpireYear(){
+//        WebElement year = driver.findElement(By.id("ExpireYear"));
+//        Select select4 = new Select(year);
+//        select4.selectByVisibleText("2033");
+//    }
+//    @Test(priority = 33)
+//    void cardCode(){
+//        driver.findElement(By.name("CardCode")).sendKeys("2748");
+//    }
+//    @Test(priority = 34)
+//    void selectContinueToConfirm(){
+//        driver.findElement(By.xpath("//*[@id=\"payment-info-buttons-container\"]/button")).click();
+//    }
+    @Test(priority = 24)
+    void closeBrowser(){
+        driver.quit();
     }
 
 }
